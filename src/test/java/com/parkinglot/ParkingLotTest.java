31d8738db6078c42d37036e18a088c528255fc31
ZHENGVI2 <Zhengvi2@oocl.com>
2024-11-22 13:46:46 +0800

    [ParkingLot][Story1] feat, Case3, return two car when fetch cars given two tickets

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -38,6 +38,21 @@ public class ParkingLotTest {
         //Then
         assertNull(fetchedCar);
     }
-
-
+    
+    @Test 
+    void should_return_tow_parked_cars_when_fetch_car_given_two_tickets(){
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        Ticket firstTicket = parkingLot.park(firstCar);
+        Ticket secondticket = parkingLot.park(secondCar);
+        //When
+        Car fetchedFirstCar = parkingLot.fetch(firstTicket);
+        Car fetchedSecondCar = parkingLot.fetch(secondticket);
+        //Then
+        assertEquals(fetchedFirstCar, firstCar);
+        assertEquals(fetchedSecondCar, secondCar);
+    }
+    
 }
