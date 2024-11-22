093d2723a3b609f30c56169ffbf26981c36bc256
ZHENGVI2 <Zhengvi2@oocl.com>
2024-11-22 11:48:43 +0800

    [ParkingLot][Story1] feat, Case1, return a ticket when park a car

---
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -0,0 +1,7 @@
+package com.parkinglot;
+
+public class ParkingLot {
+    public Ticket park(Car car) {
+        return new Ticket();
+    }
+}
---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,19 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_ticket_when_park_given_a_car(){
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        //When
+        Ticket ticket = parkingLot.park(car);
+        //Then
+        assertNotNull(ticket);
+    }
+
+}
